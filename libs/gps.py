bd4fe92daa60dfb66c83eaa6d6d1a6675178b263
Marcelo Martins <martins@cs.brown.edu>
2013-09-05 19:40:47 -0400

    Add library for access to GPS-related Android Java classes

---
+++ libs/gps.py
@@ -0,0 +1,68 @@
+from jnius import PythonJavaClass, java_method, autoclass
+
+Looper = autoclass('android.os.Looper')
+LocationManager = autoclass('android.location.LocationManager')
+PythonActivity = autoclass('org.renpy.android.PythonActivity')
+Context = autoclass('android.content.Context')
+
+class GPSListener(PythonJavaClass):
+    __slots__ = ['location_manager']
+    __javainterfaces__ = ['android/location/LocationListener']
+
+    def __init__(self, callback):
+        super(GPSListener, self).__init__()
+        self.callback = callback
+        self.location_manager = PythonActivity.mActivity.getSystemService(
+                Context.LOCATION_SERVICE)
+
+    def start(self):
+        self.location_manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                10000, 10, self, Looper.getMainLooper())
+
+    def stop(self):
+        self.location_manager.removeUpdates(self)
+
+    @java_method('()I')
+    def hashCode(self):
+        return id(self)
+
+    @java_method('(Landroid/location/Location;)V')
+    def onLocationChanged(self, location):
+        self.callback(self, 'location', location)
+
+    @java_method('(Ljava/lang/String;)V')
+    def onProviderDisabled(self, status):
+        self.callback(self, 'provider-disabled', status)
+
+    @java_method('(Ljava/lang/Object;)Z')
+    def equals(self, obj):
+        return obj.hashCode() == self.hashCode()
+
+class GPSStatusListener(PythonJavaClass):
+    __slots__ = ['location_manager']
+    __javainterfaces__ = ['android/location/GpsStatus$Listener']
+
+    def __init__(self, callback):
+        super(GPSStatusListener, self).__init__()
+        self.callback = callback
+        self.location_manager = PythonActivity.mActivity.getSystemService(
+                Context.LOCATION_SERVICE)
+
+    def start(self):
+        self.location_manager.addGpsStatusListener(self)
+
+    def stop(self):
+        self.location_manager.removeGpsStatusListener(self)
+
+    @java_method('()I')
+    def hashCode(self):
+        return id(self)
+
+    @java_method('(I)V')
+    def onGpsStatusChanged(self, event):
+        self.callback(self, 'gps-status-changed', event)
+
+    @java_method('(Ljava/lang/Object;)Z')
+    def equals(self, obj):
+        return obj.hashcode() == self.hashCode()
