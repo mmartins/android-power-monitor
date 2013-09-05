c9e01476cb596072b962d25628d6bb36053bd53a
Marcelo Martins <martins@cs.brown.edu>
2013-09-05 19:15:54 -0400

    Hardware component constants

---
+++ utils/hardware.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class Hardware(object):
+
+    CPU = "CPU"
+    GPU = "GPU"
+    WIFI = "Wifi"
+    THREEG = "3G"
+    SENSORS = "Sensors"
+    AUDIO = "Audio"
+    RADIO = "Radio"
+    GPS = "GPS"
+    LCD = "LCD"
+    OLED = "OLED"
