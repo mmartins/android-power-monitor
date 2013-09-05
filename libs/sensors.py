9d21c09aeb05224db74bfa8fa89328df1d6bae04
Marcelo Martins <martins@cs.brown.edu>
2013-09-05 19:35:21 -0400

    Add library for access to sensor-related Android Java classes

---
+++ libs/sensors.py
@@ -0,0 +1,16 @@
+from jnius import autoclass
+
+Sensor = autoclass('android.hardware.Sensor')
+PythonActivity = autoclass('org.renpy.android.PythonActivity')
+Context = autoclass('android.content.Content')
+
+class SensorsAccess(object):
+
+    @staticmethod
+    def get_sensors():
+        '''Return dictionary with (name, power) values for each Android
+        sensor'''
+        sm = PythonActivity.mActivity.getSystemService(Context.SENSOR_SERVICE)
+        sensors = sm.getSensorsList(Sensor.TYPE_ALL).toArray()
+        ret = {s.getName(): s.getPower() for s in sensors}
+        return ret
