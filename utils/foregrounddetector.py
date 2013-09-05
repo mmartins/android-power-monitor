53fdd4d7c5a15ed544123eb6ba49811c0947f2d7
Marcelo Martins <martins@cs.brown.edu>
2013-09-05 19:57:00 -0400

    Add detection for foreground-screened apps

---
+++ utils/foregrounddetector.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+try:
+    from libs.resources import ResourceAccess
+except ImportError:
+    from utils.androidhelpers.resources import ResourceAccess
+
+from utils.systeminfo import SystemInfo
+from jnius import autoclass
+
+class ForegroundDetector(object):
+
+    RunningAppProcessInfo = autoclass('android.app.ActivityManager$RunningAppProcessInfo')
+    IMPORTANCE_FOREGROUND = RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+
+    _resources = ResourceAccess()
+    _displayed_uids = []
+
+    @staticmethod
+    def get_foreground_uid():
+
+        last_displayed_ids = self._displayed_uids
+
+        # Figure out
+        apps = self._resources.get_running_apps()
+
+        # Find a valid app now. Hopefully there is only one. If there are non,
+        # return system.
+        #
+        front_uids = [SystemInfo.get_uid_for_pid(app.pid) for app in
+                apps if app.importance == self.IMPORTANCE_FOREGROUND]
+
+        self._displayed_uids.sort()
+
+        uid1 = uid2 = None
+
+        # Find an enter/exit front transition between iterations
+        for (uid1, uid2) in zip(last_displayed_uids, self._displayed_uids):
+            if uid1 == uid2:
+                continue
+            elif uid1 < uid2:
+                uid_enter = uid1
+            else:
+                uid_exit = uid2
+
+        # Find a valid application. Hopefully there is only one. If there is
+        # none, return system. If there are several, return the onde with
+        # highest UID.
+        if uid_enter == uid_exit and uid_enter in self._displayed_uids:
+            return uid_enter
+
+        return SystemInfo.AID_SYSTEM
