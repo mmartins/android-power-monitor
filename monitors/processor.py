1905f9091cfe8fdde84094c366ecf92222e49853
Marcelo Martins <martins@cs.brown.edu>
2013-09-05 19:20:03 -0400

    Add processor (collection of cores) power monitoring

---
+++ monitors/processor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from monitors.cpu import CPU
+from monitors.powermonitor import PowerMonitor
+from services.iterationdata import IterationData
+from utils.hardware import Hardware
+
+class Processor(PowerMonitor):
+
+    SYSFS_CPU_FILE = "/sys/devices/system/cpu/present"
+
+    def __init__(self, devconstants):
+        super(Processor, self).__init__(Hardware.CPU, devconstants)
+
+        self.num_cpus = self._get_num_cpus()
+        self.cpus = [CPU(i, devconstants) for i in xrange(self.num_cpus)]
+        self.has_uid_information = True
+
+    @classmethod
+    def _get_num_cpus(cls):
+        """ Return number of CPU cores from sysfs. Format should be "0-N" or
+        "0" if read successfully. Return zero cores on failures """
+
+        with open(self.SYSFS_CPU_FILE) as fp:
+            data = fp.read().strip()
+
+        minmax = data.split("-")
+
+        if len(minmax) > 0 and minmax[-1].isdigit():
+            return int(minmax[-1] + 1)
+
+        return 0
+
+    def calc_iteration(self, iter_num):
+        total = IterationData()
+        # TODO: Rewrite below using FP
+        for cpu in cpus:
+            total.add(cpu.calc_iteration(iter_num))
+        return total
