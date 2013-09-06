c958310857f32a304106523e9d6a0be9b7c1da9f
Marcelo Martins <martins@cs.brown.edu>
2013-09-05 20:01:46 -0400

    Add library for access to application-related Android Java classes

---
+++ libs/resources.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from jnius import autoclass
+
+PythonActivity = autoclass('org.renpy.android.PythonActivity')
+Context = autoclass('android.content.Context')
+
+class ResourceAccess(object):
+
+    __slot__ = ["activity_manager"]
+
+    def __init__(self):
+        self.activity_manager = PythonActivity.mActivity.getSystemService(Context.ACTIVITY_SERVICE)
+
+    def get_running_apps(self):
+        return self.activity_manager.getRunningApps().toArray()
