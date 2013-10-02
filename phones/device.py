2a116cfc83ca0917c1391d9317a89f8eb4f828bc
Marcelo Martins <martins@cs.brown.edu>
2013-10-02 11:22:40 -0400

    Added base device class

--- phones/device.py
+++ phones/device.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python
 
+class Device(object):
+
+    hardware = {
+            Hardware.CPU: None,
+            Hardware.LCD: None,
+            Hardware.OLED:None,
+            Hardware.WIFI: None,
+            Hardware.THREEG: None,
+            Hardware.GPS: None,
+            Hardware.AUDIO: None,
+            Hardware.SENSORS: None,
+    }
+
+    power_function = {
+            Hardware.CPU: None,
+            Hardware.LCD: None,
+            Hardware.OLED: None,
+            Hardware.WIFI: None,
+            Hardware.THREEG: None,
+            Hardware.GPS: None,
+            Hardware.AUDIO: None,
+            Hardware.SENSORS: None,
+    }
+
+    # This is an abstract class that should be extended and incr
+    def __init__(self):
+        raise NotImplementedError
+
+
 class DeviceConstants(object):
 
     PROVIDER_ATT = "AT&T"
