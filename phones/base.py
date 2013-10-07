b58053f45f110c0e7304eaa0a1a48a19f27716a8
Marcelo Martins <martins@cs.brown.edu>
2013-10-07 16:47:57 -0400

    Reorganize and remove redundant, shared code

--- phones/base.py
+++ phones/base.py
@@ -45,6 +45,9 @@ class Constants(object):
     WIFI_LINK_SPEEDS = []
     SENSOR_PWR_RATIOS = self._get_sensor_pwr_ratios()
 
+    # The following methods are too specific and need to be implemented
+    # according to the device in question
+
     @classmethod
     def get_3g_idle_power(cls, provider):
         raise NotImplementedError("Constants shouldn't be instantiated directly")
@@ -52,92 +55,6 @@ class Constants(object):
     @classmethod
     def get_3g_fach_power(cls, provider):
         raise NotImplementedError("Constants shouldn't be instantiated directly")
-
-    @classmethod
-    def get_cpu_power(cls, cpu_data):
-        """ Find the two closest CPU frequencies and linearly interpolate the
-        power ratio for current freq
-        """
-        if not cpu_data or len(Constants.CPU_PWR_RATIOS) == 0:
-            return 0
-        elif len(Constants.CPU_PWR_RATIOS) == 1:
-            ratio = Constants.CPU_PWR_RATIOS[0]
-        else:
-            freq = cpu_data.freq
-            if cpu_data.freq < Constants.CPU_PWR_RATIOS[0]:
-                freq = Constants.CPU_FREQS[0]
-            if cpu_data.freq > Constants.CPU_PWR_RATIOS[-1]:
-                freq = Constants.CPU_FREQS[-1]
-
-            i = cls._upper_bound(freq, Constants.CPU_FREQS)
-
-            ratio = (Constants.CPU_PWR_RATIOS[i-1] +
-                (Constants.CPU_PWR_RATIOS[i] - Constants.CPU_PWR_RATIOS[i-1]) /
-                (Constants.CPU_FREQS[i] - Constants.CPU_FREQS[i-1]) *
-                (freq - Constants.CPU_FREQS[i-1]))
-
-        return max(0, ratio * (cpu_data.usr_perc + cpu_data.sys_perc))
-
-    @classmethod
-    def get_audio_power(cls, audio_data):
-        return (Constants.AUDIO_PWR if audio_data.music else 0)
-
-    @classmethod
-    def get_gps_power(cls, gps_data):
-        res = sum(time * power for time, power in zip(gps_data.state_times,
-                Constants.GPS_STATE_PWRS))
-        return res
-
-    @classmethod
-    def get_wifi_power(cls, wifi_data):
-        ratio = 0
-
-        if not wifi_data:
-            return 0
-        if wifi_data.pwr_state == Wifi.POWER_STATE_LOW:
-            return Constants.WIFI_LOW_PWR
-        if wifi_data.pwr_state == Wifi.POWER_STATE_HIGH:
-            if len(Constants.WIFI_LINK_SPEEDS) == 1:
-                # If there is only one set speed we have to use its ratio as we
-                # have nothing else to use
-                ratio = Constants.WIFI_LINK_RATIOS[0]
-            else:
-                # Find two nearest speed/ratio pairs and linearly interpolate
-                # the ratio for this link speed
-
-                i = _upper_bound(wifi_data.speed, Constants.WIFI_LINK_SPEEDS)
-                if i == 0:
-                    i += 1
-                elif i == len(Constants.WIFI_LINK_SPEEDS):
-                    i -= 1
-
-                ratio = (Constants.WIFI_LINK_SPEEDS[i-1] +
-                        (Constants.WIFI_LINK_SPEEDS[i] -
-                         Constants.WIFI_LINK_SPEEDS[i-1]) *
-                        (wifi_data.speed - Constants.WIFI_LINK_SPEEDS[i-1]))
-
-        return max(0, Constants.WIFI_HIGH_PWR + ratio * wifi_data.tx_rate)
-
-    @classmethod
-    def get_3g_power(cls, threeg_data):
-        if not threeg_data:
-            return 0
-        if threeg_data.pwr_state == ThreeG.POWER_STATE_IDLE:
-            return Constants.get_3g_idle_power(threeg_data.provider)
-        if threeg_data.pwr_state == ThreeG.POWER_STATE_FACH:
-            return Constants.get_3g_fach_power(threeg_data.provider)
-        if threeg_data.pwr_state == ThreeG.POWER_STATE_DCH:
-            return Constants.get_3g_dch_power(threeg_data.provider)
-
-        return 0
-
-    # The following methods are too specific and need to be implemented
-    # according to the device in question
-    @classmethod
-    def get_sensor_power(cls, sensor_data):
-        res = sum(time * power for time, power in
-                zip(sensor_data.on_times.values,
-                Constants.SENSOR_PWR_RATIOS.values))
 
     @classmethod
     def get_3g_dch_power(cls, provider):
@@ -171,20 +88,6 @@ class Constants(object):
             powers[name] = power * Constants.BATTERY_VOLTAGE
 
         return powers
-
-    @classmethod
-    def _upper_bound(cls, value, list_):
-        lo = 0
-        hi = len(list_)
-
-        while (lo < hi):
-            mid = lo + (hi - lo) // 2
-            if list_[mid] <= value:
-                lo = mid + 1
-            else:
-                hi = mid
-
-        return lo
 
 
 class BaseDevice(Device):
@@ -262,10 +165,10 @@ class BasePowerCalculator(object):
         if wifi_data.pwr_state == Wifi.POWER_STATE_LOW:
             return Constants.WIFI_LOW_PWR
         if wifi_data.pwr_state == Wifi.POWER_STATE_HIGH:
-            if len(Constants.WIFI_SPEEDS) == 1:
+            if len(Constants.WIFI_LINK_SPEEDS) == 1:
                 # If there is only one set speed we have to use its ratio as we
                 # have nothing else to use
-                ratio = cls.WIFI_PWR_RATIOS[0]
+                ratio = Constants.WIFI_LINK_RATIOS[0]
             else:
                 # Find two nearest speed/ratio pairs and linearly interpolate
                 # the ratio for this link speed
@@ -278,7 +181,7 @@ class BasePowerCalculator(object):
 
                 ratio = (Constants.WIFI_LINK_SPEEDS[i-1] +
                         (Constants.WIFI_LINK_SPEEDS[i] -
-                         Constants.WIFI_SPEEDS[i-1]) *
+                         Constants.WIFI_LINK_SPEEDS[i-1]) *
                         (wifi_data.speed - Constants.WIFI_LINK_SPEEDS[i-1]))
 
         return max(0, Constants.WIFI_HIGH_PWR + ratio * wifi_data.tx_rate)
@@ -291,7 +194,7 @@ class BasePowerCalculator(object):
             return Constants.get_3g_idle_power(threeg_data.provider)
         if threeg_data.pwr_state == ThreeG.POWER_STATE_FACH:
             return Constants.get_3g_fach_power(threeg_data.provider)
-        if threeg_data.pwr_state == ThreeG.POWER_STATE_DCH:
+        if threeg_data.pwr_state == ThreeG.PO.POWER_STATE_DCH:
             return Constants.get_3g_dch_power(threeg_data.provider)
 
         return 0
@@ -304,3 +207,18 @@ class BasePowerCalculator(object):
         res = sum(time * power for time, power in
                 zip(sensor_data.on_times.values,
                 Constants.SENSOR_PWR_RATIOS.values))
+
+    @classmethod
+    def _upper_bound(cls, value, list_):
+        lo = 0
+        hi = len(list_)
+
+        while (lo < hi):
+            mid = lo + (hi - lo) // 2
+            if list_[mid] <= value:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return lo
+
