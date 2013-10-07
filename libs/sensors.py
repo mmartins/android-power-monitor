d14063dd5d14a6ff7186cdb5a4fbf62cc2535c11
Marcelo Martins <martins@cs.brown.edu>
2013-10-07 11:46:07 -0400

    Fix docstring format

--- libs/sensors.py
+++ libs/sensors.py
@@ -8,8 +8,8 @@ class SensorsAccess(object):
 
     @staticmethod
     def get_sensors():
-        '''Return dictionary with (name, power) values for each Android
-        sensor'''
+        """Return dictionary with (name, power) values for each Android
+        sensor'"""
         sm = PythonActivity.mActivity.getSystemService(Context.SENSOR_SERVICE)
         sensors = sm.getSensorList(Sensor.TYPE_ALL).toArray()
         ret = {s.getName(): s.getPower() for s in sensors}
