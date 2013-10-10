81870f79fc1973b41183a4ba756eccf245f67e5a
Marcelo Martins <martins@cs.brown.edu>
2013-10-09 23:13:39 -0400

    Fix foreground detector logic

--- utils/foregrounddetector.py
+++ utils/foregrounddetector.py
@@ -22,18 +22,18 @@ class ForegroundDetector(object):
 
         last_displayed_uids = cls._displayed_uids
 
-        # Figure out
         apps = cls._resources.get_running_apps()
 
         # Find a valid app now. Hopefully there is only one. If there are non,
         # return system.
         #
-        front_uids = [SystemInfo.get_uid_for_pid(app.pid) for app in
+        cls._displayed_uids = [SystemInfo.get_uid_for_pid(app.pid) for app in
                       apps if app.importance == cls.IMPORTANCE_FOREGROUND]
 
         cls._displayed_uids.sort()
 
-        uid1 = uid2 = None
+        uid_enter = -1
+        uid_exit = -1
 
         # Find an enter/exit front transition between iterations
         for (uid1, uid2) in zip(last_displayed_uids, cls._displayed_uids):
@@ -45,7 +45,7 @@ class ForegroundDetector(object):
                 uid_exit = uid2
 
         # Find a valid application. Hopefully there is only one. If there is
-        # none, return system. If there are several, return the onde with
+        # none, return system. If there are several, return the one with
         # highest UID.
         if uid_enter == uid_exit and uid_enter in cls._displayed_uids:
             return uid_enter
